c8f9e2c5aaa7931e221336731c3d7c14fb7bedab
JanKubesIsBest <jan.kubes07@gmail.com>
2021-12-06 17:25:34 +0100

    I fuck it 6

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const token = "OTE2Njk2OTM5NTE5NjgwNTcz.Yat6jQ.LNltHajub_G0WhRiGAcdFuO7NA0"
-const guild_id = 808649794276294666
 let prefix = require("./settings/config.json").prefix
-let runned = require("./settings/config.json").runned
-let fs = require("fs")
 
 const DiscordJs = require("discord.js")
 const { Intents } = require('discord.js')
-const mysql = require("mysql")
 
-const add_user = require("./src/database/new_user").add_user
-const new_message = require("./src/database/new_message").new_message
 
 
 const client = new DiscordJs.Client({
@@ -20,47 +14,6 @@ const client = new DiscordJs.Client({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS
     ],
-})
-const first_db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-})
-
-let db = 0;
-
-//setup
-if (runned == 0) {
-
-    console.log("Connected!");
-    first_db.query("CREATE DATABASE lolserverdiscordusers", function (err, result) {
-        if (err) throw err;
-        console.log("Database created");
-        db = mysql.createPool({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: "lolserverdiscordusers"
-        })
-        
-        var sql = "CREATE TABLE users (username VARCHAR(255), user_id int, level int, id int AUTO_INCREMENT, number_of_messages int, PRIMARY KEY (id))";
-        db.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Table created");
-            
-            fs.readFile("./settings/config.json", 'utf8', function(err, data) {
-                if (err) throw err;
-                fs.writeFile("./settings/config.json", '{"TOKEN": "OTE2Njk2OTM5NTE5NjgwNTcz.Yat6jQ.LNltHajub_G1WhRiGAcdFuO7NA0","prefix": "?","guild_id": 808649794276294666,"runned": 1}', function (err) { if (err) throw err; })
-            })
-        });
-    });
-}
-
-db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: "lolserverdiscordusers"
 })
 
 client.on("ready", () => {
@@ -77,18 +30,6 @@ client.on('guildMemberAdd', function (user) {
 client.on("messageCreate", msg => {
     const guild = msg.guild;
 
-    if (runned == 1){
-        guild.members.fetch().then(members => {
-            // Loop through every members
-            members.forEach(member => {
-                add_user(db, member.user)
-            });
-            fs.readFile("./settings/config.json", 'utf8', function(err, data) {
-                if (err) throw err;
-                fs.writeFile("./settings/config.json", '{"TOKEN": "OTE2Njk2OTM5NTE5NjgwNTcz.Yat6jQ.LNltHajub_G1WhRiGAcdFuO7NA0","prefix": "?","guild_id": 808649794276294666,"runned": 2}', function (err) { if (err) throw err; })
-            })
-        });
-    }
 
     if (!msg.author.bot) {
 
